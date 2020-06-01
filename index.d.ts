d1aefd2dc41cc91c251df581ee0bcfd20a31f6ef
mmkal <misha.kaletsky@gmail.com>
2020-05-31 21:46:48 -0400

    add types

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+type Replacer =
+  | ((val: any) => any)
+  | (string | number)[]
+
+interface StringifyOptions {
+  replacer?: 
+    | ((val: any) => any)
+    | (string | number)[]
+    | null
+  space?: string | number
+  quote?: string
+}
+
+export declare const stringify: {
+  (value: unknown, replacer?: StringifyOptions['replacer'], quote?: StringifyOptions['quote']): string
+  (value: unknown, options: StringifyOptions): string
+}
+export declare const parse: (
+  value: string,
+  reviver: (key: string, value: any) => any
+) => any
